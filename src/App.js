7c8ffd4ce978eb1661ac962baec7375f52e2285f
donfour <donovanso@yahoo.com.hk>
2018-07-29 19:13:10 +0800

    added part 3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import TextField from '@material-ui/core/TextField';
 import './App.css';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
+
+import TodoList from './components/TodoList';
+import TodoInput from './components/TodoInput';
 
 class App extends Component {
   state = {
     todos: [],
-    idCount: 0,
-    input: ''
+    idCount: 0
   }
 
   addTodo(todoName){
@@ -51,35 +38,8 @@ class App extends Component {
     return (
       <div className="App">
         <Card className="main-container">
-          <TextField
-            className='add-todo-input'
-            type='text'
-            value={this.state.input}
-            onChange={e =>
-              this.setState({input: e.target.value})
-            }
-            label="Add New Todo"
-            onKeyPress={e => {
-              if(e.key=='Enter'){
-                this.addTodo(this.state.input);
-              }
-            }}
-          />
-          <List>
-            {this.state.todos.map(todo =>
-              <ListItem>
-                <Icon>drag_handle</Icon>
-                <ListItemText
-                  primary={todo.name}
-                />
-                <ListItemSecondaryAction>
-                  <IconButton aria-label="Delete">
-                    <Icon>delete</Icon>
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            )}
-          </List>
+          <TodoInput handleSubmit={this.addTodo.bind(this)} />
+          <TodoList data={this.state.todos} handleClick={this.deleteTodo.bind(this)} />
         </Card>
       </div>
     );
